152ffe04f3a9500a0483a40cd10aa952208b07c8
Vaisakh Nair <vaisakh@ronintech.me>
2017-12-12 15:24:14 +0400

    bug fix

--- adapters/usb.js
+++ adapters/usb.js
@@ -104,31 +104,33 @@ util.inherits(USB, EventEmitter);
 USB.prototype.open = function (callback) {
     let self = this, counter = 0, index = 0;
     this.device.open();
-    this.device.interfaces.forEach((iface) => {
-        ((iface) => {
-            iface.setAltSetting((iface.altSetting, () => {
+    this.device.interfaces.forEach(function (iface) {
+        (function (iface) {
+            iface.setAltSetting(iface.altSetting, function () {
                 // http://libusb.sourceforge.net/api-1.0/group__dev.html#gab14d11ed6eac7519bb94795659d2c971
                 // libusb_kernel_driver_active / libusb_attach_kernel_driver / libusb_detach_kernel_driver : "This functionality is not available on Windows."
-                if ( 'win32' !== os.platform() ) {
+                if ( "win32" !== os.platform() ) {
                     if ( iface.isKernelDriverActive() ) {
                         try {
                             iface.detachKernelDriver();
                         } catch ( e ) {
-                            console.error('[ERROR] Could not detach kernel driver: %s', e);
+                            console.error("[ERROR] Could not detatch kernel driver: %s", e)
                         }
                     }
                 }
-                iface.claim();// must be called before using any endpoints of this interface.
-                iface.endpoints.filter((endpoint) => {
-                    if ( endpoint.direction == 'out' && !self.endpoint )
+                iface.claim(); // must be called before using any endpoints of this interface.
+                iface.endpoints.filter(function (endpoint) {
+                    if ( endpoint.direction == 'out' && !self.endpoint ) {
                         self.endpoint = endpoint;
+                    }
                 });
                 if ( self.endpoint ) {
                     self.emit('connect', self.device);
                     callback && callback(null, self);
-                } else if ( ++counter == this.device.interfaces.length && !self.endpoint )
-                    callback && callback(new Error('Cannot find endpoint from printer'));
-            }));
+                } else if ( ++counter === this.device.interfaces.length && !self.endpoint ) {
+                    callback && callback(new Error('Can not find endpoint from printer'));
+                }
+            });
         })(iface);
     });
     return this;
